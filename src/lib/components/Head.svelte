21191a69e95025049d60f183655efd588640de17
PeterSchaerli <155375474+PeterSchaerli@users.noreply.github.com>
2024-01-02 15:58:18 +0100

    Update Head.svelte

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -1,5 +1,5 @@
 <script>
-	import logo from '$lib/assets/logo.svg';
+	import logo from '$static/Logo01.png';
 	import Menu from '$lib/components/Menu.svelte';
 	import { onMount } from 'svelte';
 	import { tweened } from 'svelte/motion';
